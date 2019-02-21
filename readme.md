0a2a4c9bca6c4320a73c02d05130c3cadc3bb5a7
Swasthik shetty <swaasthik.shetty07@gmail.com>
2019-02-21 13:50:04 +0530

    read me file added and input statement added

---
+++ readme.md
@@ -0,0 +1,2 @@
+# Swap
+This is a c program to swap two numbers using third variable
--- swap.c
+++ swap.c
@@ -6,13 +6,13 @@ void swap(int a,int b)
     temp=a;
     a=b;
     b=temp;
-print("After Swapping\n Value of a=%d and b=%d",a,b);
+printf(" \n After Swapping\n Value of a=%d and b=%d",a,b);
 }
 void main()
 {
     int a,b;
-    a=10;
-    b=20;
-    print("Before Swapping\n Value of a=%d and b=%d",a,b);
+    printf("Enter the value of a and b \n");
+    scanf("%d%d",&a,&b);
+    printf("\n Before Swapping\n Value of a=%d and b=%d",a,b);
     swap(a,b);
 }
